4f19433bba49db02e8b688ba8d34df8f46f10fe2
ViniciusHansen <vinicius.carlo.hansen@gmail.com>
2021-05-12 08:50:38 -0300

    calculadora de juros ismples finalizada

--- calc_juros_simples.py
+++ calc_juros_simples.py
@@ -1,2 +1,7 @@
-valor = input('valor')
-taxa_de_juros_mensal = input('taxa em percentual')
+valor = input("valor da aplicação em reais: ")
+taxa_de_juros_mensal = input(
+    "rendimento mensal em percentual do valor aplicado:")
+valor = float(valor)
+taxa_de_juros_mensal = float(taxa_de_juros_mensal) / 100
+rendimento_mensal = valor * taxa_de_juros_mensal
+print('O Rendimento Mensal é de', rendimento_mensal, 'reais')
