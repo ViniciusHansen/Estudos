b076275500d3628e731b62c6536582709a9d3b10
ViniciusHansen <vinicius.carlo.hansen@gmail.com>
2021-05-14 09:45:40 -0300

    otimização do codigo

--- cal_juros_comp_aporte.py
+++ cal_juros_comp_aporte.py
@@ -1,15 +1,16 @@
+# input dos valores
 valor = input("valor da aplicação em reais: ")
 taxa_de_juros_mensal = input(
     "rendimento mensal em percentual:")
 aporte = input("valor dos aportes mensais:")
 tempo = input("quantidade de meses reinvestindo os juros: ")
+# Conversão de string para float
 valor = float(valor)
 tempo = float(tempo)
 aporte = float(aporte)
 taxa_de_juros_mensal = float(taxa_de_juros_mensal) / 100
+# Fórmula dos juros compostos com aportes mensais
 valor_futuro = valor + \
     (aporte * ((1 + taxa_de_juros_mensal) ** tempo - 1)) / taxa_de_juros_mensal
-valor_futuro = round(valor_futuro, 2)
-#montante_final = aporte * (1 + taxa_de_juros_mensal) ** tempo
-#montante_final = round(montante_final, 2)
-print(valor_futuro)
+# Arredondamento e print
+print(round(valor_futuro, 2))
