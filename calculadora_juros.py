d191443125982f3f984bc7358bf00659947390f7
ViniciusHansen <vinicius.carlo.hansen@gmail.com>
2021-08-13 19:48:52 -0300

    update calculators

--- calculadora_juros.py
+++ calculadora_juros.py
@@ -1,24 +1,29 @@
 # input dos valores
-valor = input("valor da aplicação em reais: ")
-juros_mensal = input("rendimento mensal em percentual:")
-aporte = input("valor dos aportes mensais:")
-tempo = input("quantidade de meses reinvestindo os juros: ")
+# valor = input("valor da aplicação em reais: ")
+# juros_mensal = input("rendimento mensal em percentual:")
+# aporte = input("valor dos aportes mensais:")
+# tempo = input("quantidade de meses reinvestindo os juros: ")
+
 # Conversão de string para float
-valor = float(valor)
-tempo = int(tempo)
-aporte = float(aporte)
-juros_mensal = float(juros_mensal) / 100
+# valor = float(valor)
+# tempo = int(tempo)
+# aporte = float(aporte)
+# juros_mensal = float(juros_mensal) / 100
+
+# Valores
+valor = 1000
+juros_mensal = 0.5 / 100
+aporte = 200
+tempo = 60  # meses
+
 # Math
 juros_aporte = round(
     valor + (aporte * ((1 + juros_mensal) ** tempo - 1)) / juros_mensal, 2)
 juros_reapply = round(valor * (1 + juros_mensal) ** tempo, 2)
 juros_simples = (valor * juros_mensal) * tempo
+
 # print
-print('\n', 'Valores aproximados:', '\n')
-print(' * Valor Final do investimento com aportes mensais e reaplicação de juros:',
-      juros_aporte, '\n')
-print(' * Valor Final do investimento apenas com reaplicação de juros:',
-      juros_reapply, '\n')
-print(' * Valor Final do investimento em juros simples:',
-      juros_simples, '\n')
-print('\n')
+print('\n', 'Valor Final do investimento: ', '\n')
+print('* aportes mensais e reaplicação de juros: R$:{} \n'.format(juros_aporte))
+print('* reaplicação de juros: R$:{} \n'.format(juros_reapply))
+print('* Rendimento em juros simples: R$:{} por mês \n'.format(juros_simples))
