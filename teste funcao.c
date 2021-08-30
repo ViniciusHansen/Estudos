e78a95b599b32e0be38fa9c0e97879859a4426a1
ViniciusHansen <vinicius.carlo.hansen@gmail.com>
2021-08-30 20:43:30 -0300

    add fibonacci function

--- teste funcao.c
+++ teste funcao.c
@@ -19,10 +19,36 @@ int fatorial(int numero)
     return fator;
 }
 
+int fibonacci(n)
+{
+    int x, y, z, count;
+    x = 0;
+    y = 1;
+    z = x + y;
+    count = 1;
+    do
+    {
+        printf("%i \n", x);
+        z = x + y;
+        x = y;
+        y = z;
+        ++count;
+        // printf("\n");
+    } while (count < n + 1);
+}
+
 int main()
 {
+    printf("\nFATORIAL \n");
     printf("%d \n", fatorial(4));
     printf("%d \n", fatorial(6));
     printf("%d \n", fatorial(8));
     printf("%d \n", fatorial(10));
+
+    printf("\nFIBONACCI n = 1\n");
+    printf("%d \n", fibonacci(1));
+    printf("\nFIBONACCI n = 3\n");
+    printf("%d \n", fibonacci(3));
+    printf("\nFIBONACCI n = 5\n");
+    printf("%d \n", fibonacci(5));
 }
