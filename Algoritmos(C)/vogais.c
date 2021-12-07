da3beac8c54e63609cd24aebb07c6b04e907e899
Vinícius Hansen <vinicius.carlo.hansen@gmail.com>
2021-12-07 20:12:12 -0300

    Add files via upload

---
+++ Algoritmos(C)/vogais.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define S 30
+
+int main(void){
+    char s[S];
+    int soma_vogal = 0, c = 0;
+
+    scanf("%[^\n]", s);
+
+    //for (int i = 0; i < S; i++){
+    //    scanf(" %c", &s[i]);
+    //}
+
+    while (s[c] != '\0') {
+        if (s[c] == 'a' || s[c] == 'A' || s[c] == 'e' || s[c] == 'E' || s[c] == 'i' || s[c] == 'I' || s[c] =='o' || s[c]=='O' || s[c] == 'u' || s[c] == 'U') {
+            soma_vogal++;
+        }
+    c++;
+    }
+
+    printf("%i", soma_vogal);
+
+    return 0;
+}
