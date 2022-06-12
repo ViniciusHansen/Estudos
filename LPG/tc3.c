98b22be4581ef292f7f2bad9d70275e5093a5615
ViniciusHansen <vinicius.carlo.hansen@gmail.com>
2022-06-12 20:52:26 -0300

    socorro n sei alocação dinamica

--- LPG/tc3.c
+++ LPG/tc3.c
@@ -40,21 +40,18 @@ typedef struct
 } contact;
 
 
-contact contato[N];
-int count=0;
-
-void busca_contato(void){
+void busca_contato(contact contato[]){
     char nome_busca[100];
     printf("Insira o nome do contato que deseja buscar:\n");
     fgets(nome_busca, 100, stdin);
     for(int i=0;i<N;i++){
         if (strcmp(nome_busca,contato[i].nome)==0){
-            printf("nome: %s\nemail: %s\ntelefone: %i\n", contato[i].nome, contato[i].email, &contato[i].telefone);
+            printf("nome: %s\nemail: %s\ntelefone: %i\n", contato[i].nome, contato[i].email, &contato[i].telefone.numero);
         }
     }
 }
 
-void aniversarios_mes(int nb_mes){
+void aniversarios_mes(int nb_mes, contact contato[]){
     for(int i=0;i<N;i++){
         if (nb_mes == contato[i].data.mes){
             printf("Nome: %s\n", contato[i].nome);
@@ -62,7 +59,7 @@ void aniversarios_mes(int nb_mes){
     }
 }
 
-void aniversarios_dia(int dia, int nb_mes){
+void aniversarios_dia(int dia, int nb_mes, contact contato[]){
     for(int i=0;i<N;i++){
         if (nb_mes == contato[i].data.mes && dia == contato[i].data.dia){
             printf("Nome: %s\n", contato[i].nome);
@@ -72,63 +69,86 @@ void aniversarios_dia(int dia, int nb_mes){
 
 
 
-void add_contato(void){
-    contact novo_contato;
+contact add_contato(contact contato[], int count){
+
+    contact *p =  (contact *) malloc(sizeof(contact));
+    if (p == NULL){
+        printf("erro no malloc\n");
+    }
 
     printf("Entre com os dados da pessoa: \nNome: \n");
-	fgets(novo_contato.nome, 20, stdin);
+	fgets(p->nome, 20, stdin);
     fflush(stdin);
     printf("Email: \n");
-	fgets(novo_contato.email, 20, stdin);
+	fgets(p->email, 20, stdin);
     fflush(stdin);
 	printf("Rua: \n");
-	fgets(novo_contato.endereco.rua, 20, stdin);
+	fgets(p->endereco.rua, 20, stdin);
     fflush(stdin);
     printf("Número: \n");
-    scanf("%i", &novo_contato.endereco.numero);
-    fflush(stdin);
+    scanf("%i", &p->endereco.numero);
+    __fpurge(stdin);
 	printf("Complemento: \n");
-	fgets(novo_contato.endereco.complemento, 20, stdin);
+	fgets(p->endereco.complemento, 20, stdin);
     fflush(stdin);
 	printf("Bairro: \n");
-	fgets(novo_contato.endereco.bairro, 20, stdin);
+	fgets(p->endereco.bairro, 20, stdin);
     fflush(stdin);
 	printf("CEP: \n");
-    scanf("%i", &novo_contato.endereco.cep);
-    fflush(stdin);
+    __fpurge(stdin);
+    scanf("%[^\n]", &p->endereco.cep);
+    __fpurge(stdin);
 	printf("Cidade: \n");
-	fgets(novo_contato.endereco.cidade, 20, stdin);
+	fgets(p->endereco.cidade, 20, stdin);
     fflush(stdin);
 	printf("Estado: \n");
-	fgets(novo_contato.endereco.estado, 20, stdin);
+	fgets(p->endereco.estado, 20, stdin);
     fflush(stdin);
 	printf("País: \n");
-	fgets(novo_contato.endereco.pais, 20, stdin);
+	fgets(p->endereco.pais, 20, stdin);
     fflush(stdin);
     printf("Fone (DDD): \n");
-	scanf("%i", &novo_contato.telefone.ddd);
-    //fflush(stdin);
+    __fpurge(stdin);
+	scanf("%[^\n]", &p->telefone.ddd);
+    __fpurge(stdin);
     printf("Fone (numero): \n");
-	scanf("%i", &novo_contato.telefone.numero);
-    fflush(stdin);
+	while (getchar() != '\n'){
+        scanf("%[^\n]", &p->telefone.numero);
+    }
+    __fpurge(stdin);
 
-
-    //contact *p =  (contact *) malloc(sizeof(contact));
-    //if (p == NULL){
-    //    printf("erro no malloc\n");
-    //}
-
-
-    contato[count]=novo_contato;
 
     count++;
-    //free(p);
-//work in progress
+    return *p;
+
+    free(p);
+}
+
+contact* troca(contact contato[], int cod, int max){
+    contact temp;
+    temp = contato[cod];
+    contato[cod] = contato[max-1];
+    contato[max-1] = temp;
+    return contato;
+}
+
+contact* rm_contato(contact contato[], int max){
+    int i, cod;
+    for (i = 0; i < max; i++){
+        printf("[%d] - %s\n", i, contato[i].nome);
+    }
+
+    printf("\n Digite o codigo do contato para deletar: ");
+    scanf("%d", &cod);
+
+    return troca(contato, cod, max);
 }
 
 
 
 int main(void){
+    int count=0;
+    contact *contato = (contact *) malloc(sizeof(contact) * (count+1));
     char nome_busca[100];
     int opcao, nb_mes, dia;
     do {
@@ -137,6 +157,7 @@ int main(void){
 		printf(" 2 - aniversarios do mes\n");
 		printf(" 3 - aniversarios do dia\n");
 		printf(" 4 - adicionar contato\n");
+		printf(" 5 - remover contato\n");
 		printf(" 0 - sair\n");
 
 		scanf("%i", &opcao);
@@ -144,18 +165,26 @@ int main(void){
         __fpurge(stdin);
 
 		switch(opcao) {
-            case 1: busca_contato();
+            case 1: busca_contato(contato);
 			   break;
             case 2: printf("escreva o mes(número):\n");
                     scanf("%i", nb_mes);
-                    aniversarios_mes(nb_mes);
+                    aniversarios_mes(nb_mes, contato);
 			   break;
             case 3: printf("escreva a data (dd/mm):\n");
                     scanf("%i/%i", dia, nb_mes);
-                    aniversarios_dia(dia, nb_mes);
+                    aniversarios_dia(dia, nb_mes, contato);
 			   break;
-			case 4: add_contato();
+			case 4:
+                    contato[count] = add_contato(contato, count);
+                    count++;
+                    contato = realloc(contato, (count+1) * sizeof(contato));
 			   break;
+            case 5:
+                    contato = rm_contato(contato, count);
+                    count--;
+                    contato = realloc(contato, (count+1) * sizeof(contato));
+               break;
 		}
 
 	} while(opcao);
