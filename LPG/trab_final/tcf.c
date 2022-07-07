217e98bfa22e83dc9a007d8da2ba2d067c06aaeb
ViniciusHansen <vinicius.carlo.hansen@gmail.com>
2022-07-07 19:16:49 -0300

    progresso no tcf

--- LPG/trab_final/tcf.c
+++ LPG/trab_final/tcf.c
@@ -28,10 +28,13 @@ int main(void){
     FILE *arq = fopen("arq.bin", "rb");
 
     do {
-		printf("Escolha uma opcao:\n");
-		printf(" 1 - Adicionar evento \n");
-		printf(" 2 - \n");
-		printf(" 0 - Sair\n");
+		printf("Escolha uma opção:\n");
+		printf(" 1 - Cadastrar um novo evento na agenda\n");
+		printf(" 2 - Mostrar todos os eventos da agenda\n");
+		printf(" 3 - Mostrar todos os eventos de uma data\n");
+		printf(" 4 - A partir da data atual, mostrar os 5 (cinco) próximos eventos\n");
+		printf(" 5 - Remover evento\n");
+		printf(" 6 - Sair do programa\n");
 
 		scanf("%i", &opcao);
         __fpurge(stdin);
@@ -45,15 +48,17 @@ int main(void){
                     scanf("%i", &nb_mes);
                     aniversarios_mes(nb_mes, contato);
 			   break;
+            case 0: free(evento);
+                    return 0;
 		}
 
 	} while(opcao);
 
 
-    return 0;
+    return 1;
 }
 
-Evento cadastrar(Evento evento, int count){
+Evento* cadastrar(Evento evento, int count){
     Evento *temp =  (Evento *) malloc(sizeof(Evento));
     if (p == NULL){
         printf("erro no malloc\n");
@@ -77,95 +82,74 @@ Evento cadastrar(Evento evento, int count){
     free(temp);
 }
 
-
-
 void mostra_eventos(Evento evento, int max){
     for(int i=0;i<max;i++){
-        printf("nome: %s\nemail: %s\ntelefone: %ls\n", contato[i].nome, contato[i].email, &contato[i].telefone.numero);
+        printf("Data: %i/%i/%i \n Horário de início: %i:%i \n Horário fim:
+                %i:%i \n Local: %s \n, Descrição: %s \n\n", &evento[i]->data.dia,
+                &evento[i]->data.mes, &evento[i]->data.ano, &evento[i]->inicio.hora,
+                &evento[i]->inicio.minuto, &evento[i]->fim.hora, &evento[i]->fim.minuto,
+                evento[i]->local, evento[i]->desc);
     }
 }
 
-void aniversarios_mes(int nb_mes, contact contato[]){
-    for(int i=0;i<N;i++){
-        if (nb_mes == contato[i].data.mes){
-            printf("Nome: %s\n", contato[i].nome);
+void eventos_hoje(Evento evento, int max){
+    int dia, mes, ano;
+    printf("Insira a data que deseja buscar (dd/mm/aaaa):\n");
+    scanf("%i/%i/%i", dia, mes, ano);
+    printf("Estes são os eventos agendados para essa data:\n");
+    for(int i=0;i<max;i++){
+        if (&evento.data.dia == dia && &evento.data.mes == mes && &evento.data.ano == ano){
+            printf("Data: %i/%i/%i \n Horário de início: %i:%i \n Horário fim:
+                %i:%i \n Local: %s \n, Descrição: %s \n\n", &evento[i]->data.dia,
+                &evento[i]->data.mes, &evento[i]->data.ano, &evento[i]->inicio.hora,
+                &evento[i]->inicio.minuto, &evento[i]->fim.hora, &evento[i]->fim.minuto,
+                evento[i]->local, evento[i]->desc);
+
         }
     }
 }
 
-void aniversarios_dia(int dia, int nb_mes, contact contato[]){
-    for(int i=0;i<N;i++){
-        if (nb_mes == contato[i].data.mes && dia == contato[i].data.dia){
-            printf("Nome: %s\n", contato[i].nome);
+void proximos_eventos(Evento evento, int count){
+    int dia, mes, ano, count_aux=0;
+    printf("Insira a data que deseja buscar (dd/mm/aaaa):\n");
+    scanf("%i/%i/%i", dia, mes, ano);
+    printf("Estes são os 5 próximos eventos a partir dessa data:\n");
+    for(int i=0;i<max;i++){
+        if (&evento.data.dia >= dia && &evento.data.mes >= mes &&
+                &evento.data.ano >= ano || count_aux >= 5 ){
+            printf("Data: %i/%i/%i \n Horário de início: %i:%i \n Horário fim:
+                %i:%i \n Local: %s \n, Descrição: %s \n\n", &evento[i]->data.dia,
+                &evento[i]->data.mes, &evento[i]->data.ano, &evento[i]->inicio.hora,
+                &evento[i]->inicio.minuto, &evento[i]->fim.hora, &evento[i]->fim.minuto,
+                evento[i]->local, evento[i]->desc);
+            count_aux++;
+        }
+    }
+}
+
+Evento* remover_evento(Evento evento[], int max){
+    int dia, mes, ano, hora, minuto;
+    printf("Insira a data que deseja buscar (dd/mm/aaaa):\n");
+    scanf("%i/%i/%i", dia, mes, ano);
+    printf("Insira o horário de início do evento a ser deletado (hh:mm):\n");
+    scanf("%i:%i", hora, minuto);
+
+    for(int i=0;i<max;i++){
+        if (&evento.data.dia == dia && &evento.data.mes == mes &&
+                &evento.data.ano == ano && &evento.inicio.hora == hora &&
+                &evento.inicio.minuto == minuto){
+            return troca(evento, i, max);
         }
     }
 }
 
 
-
+// referencia
 contact* troca(contact contato[], int cod, int max){
     contact temp;
     temp = contato[cod];
     contato[cod] = contato[max-1];
     contato[max-1] = temp;
     return contato;
-}
-
-contact* rm_contato(contact contato[], int max){
-    int i, cod;
-    for (i = 0; i < max; i++){
-        printf("[%d] - %s\n", i, contato[i].nome);
-    }
-
-    printf("\n Digite o codigo do contato para deletar: ");
-    scanf("%d", &cod);
-
-    return troca(contato, cod, max);
-}
-
-
-
-int main(void){
-    int count=0;
-    contact *contato = (contact *) malloc(sizeof(contact) * (count+1));
-    char nome_busca[100];
-    int opcao;
-    do {
-		printf("Escolha uma opcao:\n");
-		printf(" 1 - listar contatos\n");
-		printf(" 2 - aniversarios do mes\n");
-		printf(" 3 - aniversarios do dia\n");
-		printf(" 4 - adicionar contato\n");
-		printf(" 5 - remover contato\n");
-		printf(" 0 - sair\n");
-
-		scanf("%i", &opcao);
-        __fpurge(stdin);
-
-		switch(opcao) {
-            case 1: busca_contato(contato, count);
-			   break;
-            case 2: printf("escreva o mes(número):\n");
-                    scanf("%i", &nb_mes);
-                    aniversarios_mes(nb_mes, contato);
-			   break;
-            case 3: printf("escreva a data (dd/mm):\n");
-                    scanf("%i/%i", &dia, &nb_mes);
-                    aniversarios_dia(dia, nb_mes, contato);
-			   break;
-			case 4:
-                    contato[count] = add_contato(contato, count);
-                    count++;
-                    contato = realloc(contato, (count+1) * sizeof(contato));
-			   break;
-            case 5:
-                    contato = rm_contato(contato, count);
-                    count--;
-                    contato = realloc(contato, (count+1) * sizeof(contato));
-               break;
-		}
-
-	} while(opcao);
-    return 0;
 }
 
